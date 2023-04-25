2a7332137038a529757d64362e4d75f9705af56b
Vitaliy <kachur@class.ztk.org.ua>
2023-04-25 18:47:14 +0300

    lab 16

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ async function fetchQ(countPage) {
 }
 
 async function main(page) {
+  let likedPhotos = [];
   const gUsers = await fetchQ(page)
   clearAll()
   const Div = document.createElement("div")
@@ -18,10 +19,12 @@ async function main(page) {
   app.appendChild(Div)
   const namesArr = gUsers.map((user) => user.user.username)
   const fotoArr = gUsers.map((user) => user.user.profile_image.medium)
+  const backgroundColor = gUsers.map((user) => user.color)
 
   namesArr.forEach((item, index) => {
     const div = document.createElement("div")
-    div.classList.add("rozmir", "color")
+    div.classList.add("rozmir","color")
+    div.style.background = backgroundColor[index]
     const img = document.createElement("img")
     img.classList.add("border")
     img.src = fotoArr[index]
@@ -30,7 +33,7 @@ async function main(page) {
     name.textContent = item
     div.appendChild(name)
     app.appendChild(div)
-    img.addEventListener("click", () => {
+    div.addEventListener("click", () => {
       const fullScreenDiv = document.createElement("div")
       fullScreenDiv.classList.add("full_screen")
       const Close = document.createElement("div")
@@ -41,26 +44,60 @@ async function main(page) {
       })
       fullScreenDiv.appendChild(Close)
       const fullScreenImg = document.createElement("img")
-      fullScreenImg.src = gUsers[index].user.profile_image.large
+      fullScreenImg.src = gUsers[index].urls.regular
+      const likeFoto = gUsers[index].likes
       fullScreenDiv.appendChild(fullScreenImg)
       app.appendChild(fullScreenDiv)
-    })
+      const LikeP = document.createElement("div");
+      LikeP.textContent = "🤍";
+      LikeP.classList.add("white-heart");
+      fullScreenDiv.appendChild(LikeP);
+      let L = 0
+      if (likedPhotos.includes(gUsers[index].id)) {
+        LikeP.classList.add("red-heart");
+        LikeP.textContent = "❤️";
+        L = 1
+      }
+      const Like = document.createElement("div")
+      Like.classList.add("color3")
+      Like.textContent = `Кількість вподобань ${likeFoto + L}`
+      fullScreenDiv.appendChild(Like)  
+      LikeP.addEventListener("click", () => {
+        LikeP.classList.toggle("white-heart")
+        LikeP.classList.toggle("red-heart")
+        const likeFoto = gUsers[index].likes
+        if (LikeP.classList.contains("red-heart")) {
+          likedPhotos.push(gUsers[index].id)
+          Like.textContent = `Кількість вподобань ${likeFoto + 1}`
+          LikeP.textContent = "❤️"
+          LikeP.classList.add("red-heart")
+        } else {
+          likedPhotos = likedPhotos.filter((id) => id !== gUsers[index].id)
+          Like.textContent = `Кількість вподобань ${likeFoto}`
+          LikeP.textContent = "🤍"
+          LikeP.classList.add("white-heart")
+        }
+      })
   })
-  createListHtml(gUsers)
+  })
+  paginationHtml(page)
 }
-function createListHtml(list) {
-  const app = document.getElementById("app")
-  for (let i = 0; i < list.length; i++) {
-    const div = document.createElement("div")
-    app.appendChild(div)
-  }
-  paginationHtml()
-}
-function paginationHtml() {
+
+function paginationHtml(currentPage) {
   const pagesNums = Array.from(Array(10), (_, i) => i + 1)
-  const app = document.getElementById("app");
+  const app = document.getElementById("app")
   const divPaginate = document.createElement("div")
   divPaginate.classList.add("list")
+  const prevButton = document.createElement("div");
+  prevButton.textContent = "Назад"
+  prevButton.classList.add("cursor")
+  prevButton.style.margin = "0 10px"
+  prevButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+      main(currentPage - 1)
+    }
+  })
+  divPaginate.appendChild(prevButton);
   pagesNums.forEach((item) => {
     const div = document.createElement("div");
     div.textContent = item
@@ -68,12 +105,25 @@ function paginationHtml() {
     div.addEventListener("click", () => {
       main(item)
     })
+    if (item === currentPage) {
+      div.classList.add("active")
+    }
     divPaginate.appendChild(div)
   })
+  const nextButton = document.createElement("div");
+  nextButton.textContent = "Вперед"
+  nextButton.classList.add("cursor")
+  nextButton.style.margin = "0 10px"
+  nextButton.addEventListener("click", () => {
+    if (currentPage < 10) {
+      main(currentPage + 1)
+    }
+  })
+  divPaginate.appendChild(nextButton);
   app.appendChild(divPaginate)
 }
 function clearAll() {
   const app = document.getElementById("app");
   app.innerHTML = ""
 }
-main()
+main(1)
